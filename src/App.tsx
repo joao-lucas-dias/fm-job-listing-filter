f5f3444b5c9f680fe71324e4b64f58f722be7836
joao-lucas-dias <joao.lucasdias@hotmail.com>
2023-04-19 16:40:11 +0100

    Add css variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 const App = () => {
-	return <div className="App"></div>;
-}
+	return (
+		<div className="App">
+			<p>Hello World!</p>
+		</div>
+	);
+};
 
 export default App;
