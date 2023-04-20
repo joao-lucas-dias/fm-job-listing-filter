b99f2bd050eff65192d0d6f0910843957b64299e
joao-lucas-dias <joao.lucasdias@hotmail.com>
2023-04-20 18:34:07 +0100

    Add desktop syles and active states

--- src/components/JobItem/JobItem.tsx
+++ src/components/JobItem/JobItem.tsx
@@ -9,42 +9,41 @@ const JobItem: React.FC<{ jobData: Job; onFilterClick: (filter: string) => void 
 	const job: Job = props.jobData;
 
 	const generateImageName = () => {
-		return props.jobData.company.name.replace(/\s+/g, "-").replace(/\./g, "").toLowerCase();
+		return props.jobData.company.name
+			.replace(/\s+/g, "-")
+			.replace(/\./g, "")
+			.toLowerCase();
 	};
-
-	// let categoryList: string[] = [];
-	// categoryList.push(job.categories[0]);
-	// categoryList.push(job.categories[1]);
-	// job.categories[2].map((language) => categoryList.push(language));
-	// job.categories[3].map((tool) => categoryList.push(tool));
 
 	return (
 		<li className={`${classes.container} ${job.tags.featured && classes.featured}`}>
-			<img
-				src={`../../../src/assets/images/${generateImageName()}.svg`}
-				alt="Company Logo"
-				className={classes.logo}
-			></img>
-			<div className={classes.info}>
-				<p className={classes.header}>
-					<span className={classes.company}>{job.company.name}</span>
-					<span className={classes.tags}>
-						{job.tags.new && (
-							<span className={`${classes.tag} ${classes.tag_new}`}>NEW!</span>
-						)}
-						{job.tags.featured && (
-							<span className={`${classes.tag} ${classes.tag_featured}`}>FEATURED</span>
-						)}
-					</span>
-				</p>
-				<p className={classes.position}>{props.jobData.position}</p>
-				<p className={classes.details}>
-					<span>{job.details.postedAt}</span>
-					<span className={classes.dot}></span>
-					<span>{job.details.contract}</span>
-					<span className={classes.dot}></span>
-					<span>{job.details.location}</span>
-				</p>
+			<div className={classes.header}>
+				<img
+					src={`../../../src/assets/images/${generateImageName()}.svg`}
+					alt="Company Logo"
+					className={classes.logo}
+				></img>
+				<div className={classes.header_body}>
+					<p className={`${classes.body_row} ${classes.title}`}>
+						<span className={classes.company}>{job.company.name}</span>
+						<span className={classes.tags}>
+							{job.tags.new && (
+								<span className={`${classes.tag} ${classes.tag_new}`}>NEW!</span>
+							)}
+							{job.tags.featured && (
+								<span className={`${classes.tag} ${classes.tag_featured}`}>FEATURED</span>
+							)}
+						</span>
+					</p>
+					<p className={classes.position}>{props.jobData.position}</p>
+					<p className={`${classes.body_row} ${classes.details}`}>
+						<span>{job.details.postedAt}</span>
+						<span className={classes.dot}></span>
+						<span>{job.details.contract}</span>
+						<span className={classes.dot}></span>
+						<span>{job.details.location}</span>
+					</p>
+				</div>
 			</div>
 			<span className={classes.line}></span>
 			<ul className={classes.category_list}>
